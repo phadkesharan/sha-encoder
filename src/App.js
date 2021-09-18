34a6505b4ea6dca372610c5e0b29a6e8c6c526c4
Sharan Phadke <phadkesharan@gmail.com>
2021-09-18 22:47:21 +0530

    new component Heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Heading from './components/Heading';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Heading/>
     </div>
   );
 }
---
+++ src/components/TextArea.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextArea() {
+    return (
+        <div>
+            <div class="input-group input-group-lg">
+                <span class="input-group-text" id="inputGroup-sizing-lg">Input Text</span>
+                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
+            </div>
+        </div>
+    )
+}
