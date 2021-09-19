25ebd7543f95e3c8d5ae183ca2df6da72d4f4f4e
Sharan Phadke <phadkesharan@gmail.com>
2021-09-19 13:46:11 +0530

    added copy to clipBoard button
    Final changes

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Heading() {
     return (
         <div>
-            <h1>Welcome the Secure hash Generator</h1>
+            <h1>Secure hash Generator</h1>
             <div class="alert alert-success" role="alert">
                 Input any text and convert it into Hash Code using SHA - 256 Algorithm!
             </div>
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { sha256, sha224 } from 'js-sha256';
+import { sha256 } from 'js-sha256';
 
 export default function TextArea(props) {
 
     const onChange = (event) => {
         console.log(event.target.value);
-        if(event.target.value == ''){
+        if(event.target.value === ''){
             props.setResult("Write something!!")
         }
         else{
@@ -15,18 +15,23 @@ export default function TextArea(props) {
         
     }
 
+    const copyText = () =>{
+        navigator.clipboard.writeText(props.result);
+    }
+
     return (
         <div>
             <div className="container mb-3 my-3">
                 <div className="input-group input-group-lg ">
                     <span className="input-group-text" id="inputGroup-sizing-lg">Input</span>
-                    <textarea rows="2" id="inputText" type="text" onChange={onChange} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" />
+                    <textarea rows="1" id="inputText" type="text" onChange={onChange} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" />
                 </div>
             </div>
 
             <ul class="list-group mb-4 my-4 -lg">
                 <li class="list-group-item list-group-item-info">{props.result}</li>
             </ul>
+            <button type="button" onClick={copyText} class="btn btn-warning">Copy to ClipBoard</button>
 
         </div>
     )
